139a572eb5acffa5a3c8f127d0937a48f2bbe9a4
gilzoide <gilzoide@gmail.com>
2023-05-26 19:44:40 -0300

    Add custom inspector with preview for prefab pool asset/component
    These show the current counts for active pools

---
+++ Editor/APrefabPoolEditor.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Gilzoide.PrefabPool.Editor
+{
+    [CanEditMultipleObjects]
+    public abstract class APrefabPoolEditor : UnityEditor.Editor
+    {
+        public override bool HasPreviewGUI()
+        {
+            return Application.isPlaying && IsEnabled;
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            using (new EditorGUI.DisabledScope(true))
+            {
+                var pool = serializedObject.targetObject as IPrefabPool;
+                r.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Integer, GUIContent.none);
+                EditorGUI.IntField(r, "Inactive Count", pool.CountInactive);
+                r.y += r.height + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.IntField(r, "Active Count", pool.CountActive);
+                r.y += r.height + EditorGUIUtility.standardVerticalSpacing;
+                EditorGUI.IntField(r, "Total Count", pool.CountAll);
+            }
+        }
+
+        public override bool RequiresConstantRepaint()
+        {
+            return HasPreviewGUI();
+        }
+        
+        protected virtual bool IsEnabled => true;
+    }
+
+    [CustomEditor(typeof(APrefabPoolAsset<,>), true)]
+    public class APrefabPoolAssetEditor : APrefabPoolEditor {}
+
+    [CustomEditor(typeof(APrefabPoolComponent<,>), true)]
+    public class APrefabPoolComponentEditor : APrefabPoolEditor
+    {
+        protected override bool IsEnabled => (serializedObject.targetObject as MonoBehaviour).isActiveAndEnabled;
+    }
+}
