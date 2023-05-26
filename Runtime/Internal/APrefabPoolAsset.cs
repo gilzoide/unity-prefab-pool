a3c9c2a48326c5e716d2f39e1d12846a8a32b732
gilzoide <gilzoide@gmail.com>
2023-05-26 17:50:53 -0300

    Add generic implementation for prefab pool component/asset
    This avoids duplicated code (DRY!)

---
+++ Runtime/Internal/APrefabPoolAsset.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Gilzoide.PrefabPool
+{
+    public abstract class APrefabPoolAsset<T, TPool> : ScriptableObject, IPrefabPool<T>
+        where T : Object
+        where TPool : APrefabPool<T>, new()
+    {
+        [SerializeField] protected TPool _pool = new();
+
+        [Header("Prewarm")]
+        [Tooltip("Number of instances that will be spawned when the pool is created. "
+            + "Instances are spawned asynchronously. "
+            + "At most 'Objects Per Frame' objects will be prewarmed per frame.")]
+        [SerializeField, Min(0)] protected int _initialObjectCount;
+        
+        [Tooltip("Maximum number of instances that will be spawned per frame when pool is created. "
+            + "If zero, all items will be prewarmed in a single frame.")]
+        [SerializeField, Min(0)] protected int _objectsPerFrame;
+
+        public int CountAll => _pool.CountAll;
+        public int CountActive => _pool.CountActive;
+        public int CountInactive => _pool.CountInactive;
+
+        public T Prefab => _pool.Prefab;
+
+        void OnEnable()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.playModeStateChanged += mode =>
+            {
+                if (mode == UnityEditor.PlayModeStateChange.EnteredPlayMode)
+                {
+                    Prewarm();
+                }
+                else if (mode == UnityEditor.PlayModeStateChange.ExitingPlayMode)
+                {
+                    Dispose();
+                }
+            };
+#else
+            Prewarm();
+#endif
+        }
+
+        void OnDisable()
+        {
+            Dispose();
+        }
+
+        void OnDestroy()
+        {
+            Dispose();
+        }
+
+        public T Get()
+        {
+            return _pool.Get();
+        }
+
+        public PooledObject<T> Get(out T instance)
+        {
+            return _pool.Get(out instance);
+        }
+
+        public void Release(Object instance)
+        {
+            _pool.Release(instance);
+        }
+
+        public void Release(T instance)
+        {
+            _pool.Release(instance);
+        }
+
+        public void Clear()
+        {
+            _pool.Clear();
+        }
+
+        public void Prewarm()
+        {
+            _pool.Prewarm(_initialObjectCount, _objectsPerFrame);
+        }
+
+        public void Dispose()
+        {
+            _pool.Dispose();
+        }
+    }
+}
--- Runtime/PrefabPoolComponent.cs
+++ Runtime/PrefabPoolComponent.cs
@@ -1,78 +1,9 @@
 using UnityEngine;
-using UnityEngine.Pool;
-using Object = UnityEngine.Object;
 
 namespace Gilzoide.PrefabPool
 {
-    public class PrefabPoolComponent<T> : MonoBehaviour, IPrefabPool<T> where T : Object
+    public class PrefabPoolComponent<T> : APrefabPoolComponent<T, PrefabPool<T>> where T : Object
     {
-        [SerializeField] protected PrefabPool<T> _pool = new();
-
-        [Header("Prewarm")]
-        [Tooltip("Number of instances that will be spawned when the pool is created. "
-            + "Instances are spawned asynchronously. "
-            + "At most 'Objects Per Frame' objects will be prewarmed per frame.")]
-        [SerializeField, Min(0)] protected int _initialObjectCount;
-        
-        [Tooltip("Maximum number of instances that will be spawned per frame when pool is created. "
-            + "If zero, all items will be prewarmed in a single frame.")]
-        [SerializeField, Min(0)] protected int _objectsPerFrame;
-
-        public int CountAll => _pool.CountAll;
-        public int CountActive => _pool.CountActive;
-        public int CountInactive => _pool.CountInactive;
-
-        public T Prefab => _pool.Prefab;
-
-        void OnEnable()
-        {
-            Prewarm();
-        }
-
-        void OnDisable()
-        {
-            Dispose();
-        }
-
-        void OnDestroy()
-        {
-            Dispose();
-        }
-
-        public T Get()
-        {
-            return _pool.Get();
-        }
-
-        public PooledObject<T> Get(out T instance)
-        {
-            return _pool.Get(out instance);
-        }
-
-        public void Release(Object instance)
-        {
-            _pool.Release(instance);
-        }
-
-        public void Release(T instance)
-        {
-            _pool.Release(instance);
-        }
-
-        public void Clear()
-        {
-            _pool.Clear();
-        }
-
-        public void Prewarm()
-        {
-            _pool.Prewarm(_initialObjectCount, _objectsPerFrame);
-        }
-
-        public void Dispose()
-        {
-            _pool.Dispose();
-        }
     }
 
     public class PrefabPoolComponent : PrefabPoolComponent<GameObject>
